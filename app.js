b09a43848afffb87e1d4cebb6788bb0cc7ae9f90
rival5 <viralraval5@gmail.com>
2019-03-18 14:35:48 +0530

    Upload image from local machine and added mLab dbURI

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var express                 =  require("express"),
-    app                     =  express(),
-    bodyParser              =  require("body-parser"),
-    mongoose                =  require("mongoose"),
-    flash                   =  require("connect-flash"),
-    passport                =  require("passport"),
-    LocalStrategy           =  require("passport-local"),
-    passportLocalMongoose   =  require("passport-local-mongoose"),
-    methodOverride          =  require("method-override"),
-    Campground              =  require("./models/campground"),
-    Comment                 =  require("./models/comment"),
-    User                    =  require("./models/user"),
-    seedDB                  =  require("./seeds");
-    
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  flash = require('connect-flash'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local'),
+  passportLocalMongoose = require('passport-local-mongoose'),
+  methodOverride = require('method-override'),
+  Campground = require('./models/campground'),
+  Comment = require('./models/comment'),
+  User = require('./models/user'),
+  seedDB = require('./seeds');
 
 // requiring routes
-var commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes    = require("./routes/index");
+var commentRoutes = require('./routes/comments'),
+  campgroundRoutes = require('./routes/campgrounds'),
+  indexRoutes = require('./routes/index');
 
+const keys = require('./config/keys');
 
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_campv12";
-mongoose.connect(url, { useNewUrlParser: true });
-mongoose.set("useFindAndModify", false);
-app.use(bodyParser.urlencoded({extended : true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
+var dburl = process.env.DATABASEURL || keys.database.dbURI;
+mongoose.connect(dburl, { useNewUrlParser: true });
+mongoose.set('useFindAndModify', false);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 app.use(flash());
 // seedDB(); // seed the database
-var Campground = require("./models/campground");
+var Campground = require('./models/campground');
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "i love man utd",
+app.use(
+  require('express-session')({
+    secret: 'i love man utd',
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -46,23 +48,23 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // keeps track if user is logged in or not
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
 });
 
 app.use(indexRoutes);
 app.use(campgroundRoutes);
 app.use(commentRoutes);
 
-// var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 // app.listen(port, process.env.IP, function () {
 //     console.log("Server Started...");
 // });
 
-app.listen(3000, function(){
-    console.log("server started on port 3000");
+app.listen(port, function() {
+  console.log('server started on port 3000');
 });
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Download and run instructions
+
+`git clone https://github.com/rival5/yelp_camp.git`
+
+`cd yelp-camp`
+
+`npm install`
+
+Run the app in its own terminal tab.
+
+`node app.js` or `npm start`
+
+Database is created and stored online in mLab.
